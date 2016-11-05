82faddd8f12531d357b3649e48c54eeb60c94efb
Tim Lee <TLee@Tims-Air.lan>
2016-11-04 19:02:58 -0700

    added card material ui component to recipe profile

--- src/ui/recipeProfile.js
+++ src/ui/recipeProfile.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router'
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card'
+import FlatButton from 'material-ui/FlatButton'
 
 // const RecipeProfileContainer = React.createClass({
 // 	getInitialState: function() {
@@ -13,12 +15,31 @@ import { Link } from 'react-router'
 // 	}
 // })
 
-export default React.createClass({
-	render: function() {
-		return (
-			<div className="recipeProfileHeader">Recipe Profile</div>
-		)
-	}
-})
+const CardExampleWithAvatar = () => (
+  <Card>
+    <CardHeader
+      title="URL Avatar"
+      subtitle="Subtitle"
+      avatar="images/jsa-128.jpg"
+    />
+    <CardMedia
+      overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+    >
+      <img src="http://maquinnasuperheroes.pbworks.com/f/1347855554/banner-corporate.jpg" />
+    </CardMedia>
+    <CardTitle title="Card title" subtitle="Card subtitle" />
+    <CardText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+      Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+      Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+    </CardText>
+    <CardActions>
+      <FlatButton label="Action1" />
+      <FlatButton label="Action2" />
+    </CardActions>
+  </Card>
+);
 
-// export default RecipeProfileContainer
+export default CardExampleWithAvatar;
+
