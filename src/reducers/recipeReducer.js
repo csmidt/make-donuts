87165637c345fc19f70cd67e219ea1a82fe7125e
Tim Lee <TLee@Tims-MacBook-Air.local>
2016-11-06 09:16:47 -0800

    updated reducer for 'ADD_RECIPE' action

--- src/reducers/recipeReducer.js
+++ src/reducers/recipeReducer.js
@@ -7,13 +7,14 @@ export default function(state=DefaultState, action) {
 	switch(action.type) {
 		
 		case 'GET_RECIPES':
-			return {...state, recipes:action.recipes}
+			return {...state, recipes: action.recipes}
 
 
 		case 'GET_RECIPE': 
-			return {...state, recipe:action.recipe}
+			return {...state, recipe: action.recipe}
 
-		
+		case 'ADD_RECIPE':
+			return {...state, recipe: action.recipe}
 
 		default:
 			return state
